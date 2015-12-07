6da34dfd9cbf6a66ff2caadce8f9e0d396862d50
Clodéric Mars <cloderic.mars@craft.ai>
2015-12-07 10:46:35 +0100

    Creating actions matching the alarm.bt behavior

--- src/main.js
+++ src/main.js
@@ -1,26 +1,41 @@
 import craftai from 'craft-ai-client-js';
 import loadCfg from './loadCfg';
 
+const ALARM_SWITCH_LOWER_BOUND = 1500;
+const ALARM_SWITCH_UPPER_BOUND = 10000;
+
 loadCfg()
   .then(config => craftai(config))
   .then(instance => {
     console.log(`'${instance.id}' successfully created!`);
-    return instance.createAgent('bts/hello_world.bt', {name: 'juliette'})
-      .then(agent => console.log(`agent #${agent.id} created.`))
-      .then(() => instance.createAgent('bts/hello_world.bt', {name: 'romeo'}))
+    return instance.createAgent('bts/alarm.bt')
       .then(agent => console.log(`agent #${agent.id} created.`))
       .then(() => instance.registerAction(
-        'HelloWorld',
+        'WaitForAlarmStateChange',
         (requestId, agentId, input, success, failure) => {
-          console.log(`#${agentId}: "Hello World!"`);
-          success();
+          console.log(`#${agentId}: "Waiting for alarm state change."`);
+          setTimeout(() => {
+            let newState = !input.previousState;
+            if (newState) {
+              console.log(`#${agentId}: "Alarm started."`);
+            }
+            else {
+              console.log(`#${agentId}: "Alarm stopped."`);
+            }
+
+            success({
+              state: newState
+            });
+          }, Math.floor(Math.random() * (ALARM_SWITCH_UPPER_BOUND - ALARM_SWITCH_LOWER_BOUND + 1) + ALARM_SWITCH_LOWER_BOUND))
         }))
+      .then(() => console.log(`'WaitForAlarmStateChange' registered`))
       .then(() => instance.registerAction(
-        'SayMyName',
+        'NotifyAlarm',
         (requestId, agentId, input, success, failure) => {
-          console.log(`#${agentId}: "My name is ${input.name}."`);
+          console.log(`#${agentId}: "Alarm Confirmed!"`);
           success();
         }))
+      .then(() => console.log(`'NotifyAlarm' registered`))
       .then(() => instance.update(500))
       .catch((err) => {
         console.log(`Error during the instance lifetime, check 'https://workbench.craft.ai/instances/${instance.cfg.owner}/${instance.cfg.name}/${instance.cfg.version}/${instance.id}/monitor' for further information.`);
